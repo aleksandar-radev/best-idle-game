58ac4e8cf8c3417f350ef1d3c2480ea1090e73e4
Aleksandar Radev <alexander.radev.96@gmail.com>
2021-08-05 14:01:31 +0300

    add upgrades

---
+++ src/components/Container/Upgrades/Upgrades.vue
@@ -0,0 +1,25 @@
+<template lang="">
+<div>as
+</div>
+</template>
+
+<script>
+//  import MenuItem from './GameHeaderMenuItem.vue'
+
+export default {
+  name: 'Upgrades',
+  components: {},
+  props: {},
+  methods: {},
+  data () {
+    return {}
+  },
+  created () {
+    return {}
+  }
+}
+</script>
+
+<style module lang="scss">
+
+</style>
