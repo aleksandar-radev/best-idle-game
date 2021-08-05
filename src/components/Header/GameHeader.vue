72b91a7d4cb56041551085b53cf8fc147e170c0d
Aleksandar Radev <alexander.radev.96@gmail.com>
2021-08-05 14:34:47 +0300

    add select-none to menu texts

--- src/components/Header/GameHeader.vue
+++ src/components/Header/GameHeader.vue
@@ -3,7 +3,7 @@
   <MenuItem v-for="item in menuItems"
   @click="setActive(item.name)" :key="item" :name=item.name
   :class="this.$store.state.activeMenu == item.name ? 'bg-green-400' : ''"
-  class="hover:bg-green-200 text-xl overflow-hidden pl-3 select-none"/>
+  class="hover:bg-green-200 text-xl overflow-hidden px-3 select-none"/>
 </header>
 </template>
 
