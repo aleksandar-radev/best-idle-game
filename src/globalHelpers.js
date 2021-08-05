75096790095d3d1c86acc19e6c88560c2a481227
Aleksandar Radev <alexander.radev.96@gmail.com>
2021-08-05 13:35:04 +0300

    add global functions mixin

---
+++ src/globalHelpers.js
@@ -0,0 +1,7 @@
+export default {
+  methods: {
+    getHeaderHeight () {
+      return document.querySelector('header').offsetHeight
+    }
+  }
+}
--- src/main.js
+++ src/main.js
@@ -3,8 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './assets/styles/tailwind.css'
+import global from './globalHelpers'
 
 const app = createApp(App)
-app.config.globalProperties.$store = store
-
+app.mixin(global)
 app.use(store).use(router).mount('#app')
